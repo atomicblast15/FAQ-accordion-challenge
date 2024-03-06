c40e244941ce0d9dc94482ff40dea60afbc2f4fa
atomicblast15 <63103243+atomicblast15@users.noreply.github.com>
2024-03-06 16:59:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
 var acc = document.getElementsByClassName("question");
-var i;
+console.log({ acc });
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+function kelev() {
+  console.log("i run 1 ", acc.length);
+  for (let i = 0; i < acc.length; i++) {
+    console.log("i run 2");
+    acc[i].addEventListener("click", function () {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
-    var answer = document.getElementsByClassName("answer");
-    if (answer.style.display === "block") {
-      answer.style.display = "none";
-    } else {
-      answer.style.display = "block";
-    }
-  });
+      /* Toggle between hiding and showing the active panel */
+      var answers = document.getElementsByClassName("answer");
+      if (answers[i].style.display === "block") {
+        answers[i].style.display = "none";
+      } else {
+        answers[i].style.display = "block";
+      }
+    });
+  }
 }
+kelev();
